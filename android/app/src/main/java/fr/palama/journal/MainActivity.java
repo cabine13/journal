6ea8023949fecfabdaaa792fb34e357ddb739924
cabine13 <frbos@free.fr>
2021-04-07 11:06:17 +0200

    First commit

---
+++ android/app/src/main/java/fr/palama/journal/MainActivity.java
@@ -0,0 +1,6 @@
+package fr.palama.journal;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/domain/auth/value_objects.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+
+import '../core/failures.dart';
+import '../core/value_objects.dart';
+import '../core/value_validators.dart';
+
+class EmailAddress extends ValueObject<String> {
+  factory EmailAddress(String input) {
+    assert(input != null);
+    return EmailAddress._(
+      validateEmailAddress(input),
+    );
+  }
+
+  const EmailAddress._(this.value) : assert(value != null);
+
+  @override
+  final Either<ValueFailure<String>, String> value;
+}
+
+class Password extends ValueObject<String> {
+  factory Password(String input) {
+    assert(input != null);
+    return Password._(
+      validatePassword(input),
+    );
+  }
+
+  const Password._(this.value);
+
+  @override
+  final Either<ValueFailure<String>, String> value;
+}
